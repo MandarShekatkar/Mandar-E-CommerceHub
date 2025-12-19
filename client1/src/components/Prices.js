1c7dac9e1b03dd905857e19677643babd8e4f2b7
Mandar Shekatkar <mandarshekatkar007@gmail.com>
2025-12-19 17:26:16 +0530

    Initial commit - MERN E-commerce Final Year Project

---
+++ client1/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "₹0 to 999",
+    array: [0, 19],
+  },
+  {
+    _id: 1,
+    name: "₹999 to 4999",
+    array: [20, 39],
+  },
+  {
+    _id: 2,
+    name: "₹4999 to 19999",
+    array: [40, 59],
+  },
+  {
+    _id: 3,
+    name: "₹19999 to 49999",
+    array: [60, 79],
+  },
+  {
+    _id: 4,
+    name: "₹49999 to 99999",
+    array: [80, 99],
+  },
+  {
+    _id: 4,
+    name: "₹99999 or more",
+    array: [100, 9999],
+  },
+];
